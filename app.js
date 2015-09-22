9af0afa4667b48ac0158fdcd7104929d7f079b6f
Ricardo Barros <ricardofbarros@hotmail.com>
2015-09-22 18:22:52 +0100

    add functionality to app

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
 // Dependencies
 var net = require('net')
 var config = require('./config')
+var piblaster = require('pi-blaster.js')
 
 function boot () {
   var client = net.createConnection(config.gk.port, config.gk.host)
 
   client.on('connect', function () {
+    // Try to authenticate with the GK
+    client.write(config.secureKey + '\r\n')
+  })
 
+  client.on('data', function (data) {
+    data = data.toString().replace(/\r?\n/, '')
+
+    switch (data) {
+      case 'open':
+        moveLeft()
+        setTimeout(function () {
+          moveRight()
+        }, 1000)
+        break
+      default:
+        moveRight()
+    }
   })
 
   client.on('close', function () {
+    // Restart on close
     setTimeout(function () {
       boot()
     }, config.reconnectTimeoutMs)
   })
+}
+
+function moveLeft () {
+  piblaster.setPwm(config.gpioPin, 0.06)
+}
+
+function moveRight () {
+  piblaster.setPwm(config.gpioPin, 0.24)
 }
 
 // Start keymaster
